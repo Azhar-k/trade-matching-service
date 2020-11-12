9b59e49d8e1e4cde12309f241efc84b1882e3fcb
Azhar-k <azharazvtp@gmail.com>
2020-11-12 15:33:05 +0530

    initialcommit

---
+++ src/main/java/com/baton/azhar/tradematchingservice/TradeMatchingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baton.azhar.tradematchingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMatchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeMatchingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baton/azhar/tradematchingservice/TradeMatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baton.azhar.tradematchingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMatchingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
